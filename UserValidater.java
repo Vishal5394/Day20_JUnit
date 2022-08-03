5eeba0b684285bd71550a3eaf240e8d002365489
vishal5394 <vishaljadyal@gmail.com>
2022-08-03 18:00:58 +0530

    Added:Validate The First Name

--- UserValidater.java
+++ UserValidater.java
@@ -1,9 +1,25 @@
 package com.BridgeLabz.Junit;
 
-public class UserValidater {
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
+public class UserValidater {
+	
+	public void validateName(String name){
+        Pattern pattern=Pattern.compile("^[A-Z][a-zA-Z]{2,}");
+        boolean matches = pattern.matcher(name).matches();
+        if (matches){
+            System.out.println(name+" is valid");
+        }else
+            System.out.println(name+" is not valid");
+	  }
 	public static void main(String[] args) {
-		System.out.println("Welcome To JUNIT Program");
+		UserValidater regex=new UserValidater();
+	    Scanner scanner=new Scanner(System.in);
+	    System.out.println("Enter first name :");
+	    String firstName=scanner.next();
+	    regex.validateName(firstName);
+ }
 	}
 
-}
+
